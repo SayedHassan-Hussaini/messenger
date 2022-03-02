ec867290810b43af78bd9dd991d8833a83ef2b0d
sayedhassan-husssaini <sh.hussaini1378@gmail.com>
2022-03-02 10:10:52 +0430

    design login page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,31 @@
-import React from "react"
+import React from "react";
+import { Icon } from "@iconify/react";
+import style from "./style.module.css";
+import { Card } from "react-bootstrap";
 
-const Login=()=>{
-    return(
-        <>Login Component</>
-    )
-}
+const Login = () => {
+  return (
+    <div className={`${style.login}`}>
+        <button
+          className={`${style.btnGoogle} mt-4`}
+          type="button"
+          // onClick={renderProps.onClick}
+          // disabled={renderProps.disabled}
+        >
+          <Icon className={style.googleIcon} icon="flat-color-icons:google" />
+          Login with Google
+        </button>
+        <br/>
+        <button
+          className={`${style.btnGoogle} mt-4`}
+          type="button"
+          // onClick={renderProps.onClick}
+          // disabled={renderProps.disabled}
+        >
+          <Icon className={style.googleIcon} icon="logos:facebook" />
+          Login with Google
+        </button>
+    </div>
+  );
+};
 export default Login;
